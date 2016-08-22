4611a7990b02da5fbad0cb6558a1a0c417e2ef6a
gxnualbert <albert.chenwesoft@gmail.com>
2016-08-22 23:14:59 +0800

    Initial commit

---
+++ src/model/User.java
@@ -0,0 +1,34 @@
+package model;
+
+/**
+ * Created by albert.chen on 8/22/2016.
+ */
+public class User {
+    private int id;
+    private String username;
+    private String  password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
